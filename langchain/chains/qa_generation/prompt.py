af5281dac34a222140ce7267e9c07149b2daf411
tetsugps <imagawa77@gmail.com>
2023-04-26 16:21:20 +0900

    Update prompt.py

--- langchain/chains/qa_generation/prompt.py
+++ langchain/chains/qa_generation/prompt.py
@@ -18,6 +18,7 @@ When coming up with this question/answer pair, you must respond in the following
 ```
 
 Everything between the ``` must be valid json.
+Answer in Japanese.
 """
 templ2 = """Please come up with a question/answer pair, in the specified JSON format, for the following text:
 ----------------
